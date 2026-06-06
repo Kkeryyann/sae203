a202afae4d023ba9153d6b4d82d0f7d92f404fbe
Kkeryyann <151188794+Kkeryyann@users.noreply.github.com>
2026-06-06 09:45:39 +0000

    Update database connection method in table1_new_form.php
    Refactored database connection to use environment variables for credentials.

--- admin/table1_new_form.php
+++ admin/table1_new_form.php
@@ -1,4 +1,8 @@
         <?php
+            $username = getenv('DB_USER');
+            $password = getenv('DB_PASSWORD');
+            $dbname = getenv('DB_NAME');
+
             // Appel du bloc Header et du Menu
             require ('../header.php');
         ?>
@@ -11,7 +15,7 @@
             <h1>ajouter une <span>ferrari</span></h1>
 
             <?php
-                $mabd = new PDO('mysql:host=sae203-bd-mpp9yr;dbname=sae203Base;charset=UTF8;', 'sae203User', 'X4bRPsfIJn7A7yJOadRX');
+                $mabd = new PDO('mysql:host=sae203-bd-mpp9yr;dbname=' . getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASSWORD'));
                 $mabd->query('SET NAMES utf8;');
                 $req = "SELECT * FROM Voitures";
                 $resultat = $mabd->query($req);
