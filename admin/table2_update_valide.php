ff7a008da4f8e5665d50051aa48699cff8b6d683
Kkeryyann <151188794+Kkeryyann@users.noreply.github.com>
2026-06-06 09:39:24 +0000

    Refactor database connection to use environment variables

--- admin/table2_update_valide.php
+++ admin/table2_update_valide.php
@@ -1,4 +1,8 @@
     <?php
+		 $username = getenv('DB_USER');
+         $password = getenv('DB_PASSWORD');
+         $dbname = getenv('DB_NAME');
+
         require ('../header.php');
     ?>
 
@@ -19,7 +23,7 @@
 			$prenom=$_POST['prenom'];
 			$nationalite=$_POST['nationalite'];
 
-            $mabd = new PDO('mysql:host=sae203-bd-mpp9yr;dbname=sae203Base;charset=UTF8;', 'sae203User', 'X4bRPsfIJn7A7yJOadRX');
+            $mabd = new PDO('mysql:host=sae203-bd-mpp9yr;dbname=' . getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASSWORD'));
             $mabd->query('SET NAMES utf8;');
             $req = 'UPDATE Designer SET designer_nom="'.$nom.'", designer_prenom="'.$prenom.'", designer_nationalite="'.$nationalite.'" WHERE designer_id='.$id;
 
