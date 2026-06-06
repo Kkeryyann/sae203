49b2834c179d5c2616682b31f7e97b2d67d3b01e
Kkeryyann <151188794+Kkeryyann@users.noreply.github.com>
2026-06-06 09:06:58 +0000

    Refactor database connection to use environment variables

--- listing.php
+++ listing.php
@@ -1,4 +1,8 @@
         <?php
+
+            $username = getenv('DB_USER');
+            $password = getenv('DB_PASSWORD');
+            $dbname = getenv('DB_NAME');
             // Appel du bloc Header et du Menu
             require ('header.php');
         ?>
@@ -16,7 +20,7 @@
             </button>
 
             <?php
-                $mabd = new PDO('mysql:host=sae203-bd-mpp9yr;dbname=sae203Base;charset=UTF8;', 'sae203User', 'X4bRPsfIJn7A7yJOadRX');
+                $mabd = new PDO('mysql:host=sae203-bd-mpp9yr;dbname=' . getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASSWORD'));
                 $mabd->query('SET NAMES utf8;');
                 $req = "SELECT * FROM Voitures INNER JOIN Designer ON Voitures._designer_id = Designer.designer_id";
 
