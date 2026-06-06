6b57678b73884fe1db42b26f195169d651840130
Kkeryyann <151188794+Kkeryyann@users.noreply.github.com>
2026-06-06 09:40:57 +0000

    Refactor database connection to use environment variables

--- admin/table2_new_valide.php
+++ admin/table2_new_valide.php
@@ -1,4 +1,8 @@
     <?php
+	    $username = getenv('DB_USER');
+        $password = getenv('DB_PASSWORD');
+        $dbname = getenv('DB_NAME');
+
         // Appel du bloc Header et du Menu
         require ('../header.php');
     ?>
@@ -21,7 +25,7 @@
 			$prenom=$_POST['prenom'];
 			$nationalite=$_POST['nationalite'];
 
-            $mabd = new PDO('mysql:host=sae203-bd-mpp9yr;dbname=sae203Base;charset=UTF8;', 'sae203User', 'X4bRPsfIJn7A7yJOadRX');
+            $mabd = new PDO('mysql:host=sae203-bd-mpp9yr;dbname=' . getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASSWORD'));
             $mabd->query('SET NAMES utf8;');
 			$req = 'INSERT INTO Designer(designer_nom,designer_prenom,designer_nationalite) 
             VALUES ("'.$nom.'","'.$prenom.'","'.$nationalite.'")';
