146d2eb99462ba981fe4de3b87644675802247ae
Kkeryyann <151188794+Kkeryyann@users.noreply.github.com>
2026-06-06 09:45:09 +0000

    Refactor database connection to use environment variables

--- admin/table1_new_valide.php
+++ admin/table1_new_valide.php
@@ -1,4 +1,8 @@
     <?php
+	    $username = getenv('DB_USER');
+        $password = getenv('DB_PASSWORD');
+        $dbname = getenv('DB_NAME');
+
         // Appel du bloc Header et du Menu
         require ('../header.php');
     ?>
@@ -27,7 +31,7 @@
 			$prix=$_POST['prix'];
 			$numdesigner=$_POST['designer'];
 
-            $mabd = new PDO('mysql:host=sae203-bd-mpp9yr;dbname=sae203Base;charset=UTF8;', 'sae203User', 'X4bRPsfIJn7A7yJOadRX');
+            $mabd = new PDO('mysql:host=sae203-bd-mpp9yr;dbname=' . getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASSWORD'));
             $mabd->query('SET NAMES utf8;');
 
             //vérification du format de l'image téléchargée
