cad53356b4c2f087b77ab1c7c667177c284d49d5
Kkeryyann <151188794+Kkeryyann@users.noreply.github.com>
2026-06-06 09:47:05 +0000

    Refactor database connection to use environment variables

--- admin/table1_delete.php
+++ admin/table1_delete.php
@@ -1,4 +1,8 @@
         <?php
+            $username = getenv('DB_USER');
+            $password = getenv('DB_PASSWORD');
+            $dbname = getenv('DB_NAME');
+
             // Appel du bloc Header et du Menu
             require ('../header.php');
         ?>
@@ -13,7 +17,7 @@
                 // recupérer dans l'url l'id de l'album à supprimer
                 $num =$_GET['num'];
 
-                $mabd = new PDO('mysql:host=sae203-bd-mpp9yr;dbname=sae203Base;charset=UTF8;', 'sae203User', 'X4bRPsfIJn7A7yJOadRX');
+                $mabd = new PDO('mysql:host=sae203-bd-mpp9yr;dbname=' . getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASSWORD'));
                 $mabd->query('SET NAMES utf8;');
 
                 // tapez ici la requete de suppression de l'album dont l'id est passé dans l'url
